574bf09d402c5358bc6236a158973c573fbbc0c7
GeorgiosKalantzis <georgekalantzis997@gmail.com>
2020-12-29 22:56:09 +0200

    OneVSall classification

--- data_exploration.py
+++ data_exploration.py
@@ -9,6 +9,14 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.feature_selection import SelectKBest
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+from sklearn.cluster import DBSCAN
 
 
 data = pd.read_csv('swipes.csv', header = 0)
@@ -18,11 +26,25 @@ player = data['playerID']
 data.drop(labels=['playerID'], axis=1,inplace = True)
 data.insert(0, 'playerID', player)
 
+# Drop swipe id
 data.drop(labels=['id'], axis=1,inplace = True)
+
+
+# Drop information on users with only one swipe
+# Here oversampling is needed for these users
+index1 = data[data['playerID'] == 'y2opfq8'].index
+index2 = data[data['playerID'] == 'bh54lsq'].index
+index3 = data[data['playerID'] == 'lvrishm'].index
+
+data.drop(index1 , inplace=True)
+data.drop(index2 , inplace=True)
+data.drop(index3 , inplace=True)
+
 
 # creating instance of labelencoder
 labelencoder = LabelEncoder()
 
+# Encoders for these features, in the future probably we will drop screen , deviceOS, userGender but not direction
 data['screen'] = labelencoder.fit_transform(data['screen'])
 data['deviceOS'] = labelencoder.fit_transform(data['deviceOS'])
 data['direction'] = labelencoder.fit_transform(data['direction'])
@@ -30,8 +52,16 @@ data['userGender'] = labelencoder.fit_transform(data['userGender'])
 
 y = data.iloc[0:,0]
 x = data.iloc[0:,1:]
+# Standard scaling also for the encoded features
+x = StandardScaler().fit_transform(x)
 
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state = 2020, stratify=y)   
 
+# One versus All svm classifier
+clf = OneVsRestClassifier(SVC(),n_jobs=-1).fit(X_train, y_train)
+prediction = clf.predict(X_test)
+
+"""
 # Entropy with respect to tagrget
 importances = mutual_info_classif(x,y)
 feat_importances = pd.Series(importances, x.columns[0:len(x.columns)])
@@ -51,6 +81,6 @@ plt.show()
 # display the retained features.
 features = x.columns[selection.get_support()]
 print(features)
-
+"""
 
 
